d515e87f7b300245d84becc4a205e1e7915dceb6
akashtech-source <mr.ifranakash@gmail.com>
2021-07-06 23:56:44 +0600

    second update

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     console.log('connection error', err);
   const partnerCollection = client.db("bright").collection("customer");
+  const reviewCollection = client.db("bright").collection("review");
+  const grid1Collection = client.db("bright").collection("grid1");
   // perform actions on the collection object
   console.log('database connected');
 
@@ -31,10 +33,46 @@ client.connect(err => {
     })
   })
 
+  app.get('/review', (req, res) => {
+    reviewCollection.find()
+    .toArray((err, items) => {
+        res.send(items)
+        console.log('from db', items);
+    })
+  })
+
+  app.get('/grid1', (req, res) => {
+    grid1Collection.find()
+    .toArray((err, items) => {
+        res.send(items)
+        console.log('from db', items);
+    })
+  })
+
     app.post('/addPartner', (req, res) => {
         const newPartner = req.body;
         console.log('adding new partner', newPartner);
         partnerCollection.insertOne(newPartner)
+        .then(result => {
+            console.log('inserted count',result.insertedCount);
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    app.post('/addReview', (req, res) => {
+        const newReview = req.body;
+        console.log('adding new review', newReview);
+        reviewCollection.insertOne(newReview)
+        .then(result => {
+            console.log('inserted count',result.insertedCount);
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    app.post('/addGrid1', (req, res) => {
+        const newGrid1 = req.body;
+        console.log('adding new review', newGrid1);
+        grid1Collection.insertOne(newGrid1)
         .then(result => {
             console.log('inserted count',result.insertedCount);
             res.send(result.insertedCount > 0)
